f5f53b6bfe5e1bbd7f0bcf1dad0f914ed3bfb6a8
micha <jw0711qt@go.minnstate.edu>
2020-10-28 03:24:31 -0500

    wishlist django

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 
+# Create your views here.
 def place_list(request):
 
     if request.method == 'POST':
-        
+        # Create a new place
 
-        form = NewPlaceForm(request.POST) 
-        place = form.save() 
-        if form.is_valid(): 
-            place.save()  
-            return redirect('place_list') 
+        form = NewPlaceForm(request.POST) # Creating a form from data  in the request
+        place = form.save() # Creating a model object from form 
+        if form.is_valid(): # Validation against the DB constraits 
+            place.save()  # save places entered to DB
+            return redirect('place_list') # Reloads home page
 
 
     places = Place.objects.filter(visited=False).order_by('name')
-    new_place_form = NewPlaceForm() 
+    new_place_form = NewPlaceForm() # used to create html
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_from': new_place_form})
 
 
@@ -26,8 +27,9 @@ def places_visited(request):
 
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
-        
+        #place = Place.objects.get(pk=place_pk)
         place = get_object_or_404(Place, pk=place_pk)
         place.visited = True
         place.save()
     return redirect('place_list')
+
